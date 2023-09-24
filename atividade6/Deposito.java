257b7a918ba154a62b1d0f2bcadcdfc9f8556a93
Lucas Andrade Torres <127760173+Andrade098@users.noreply.github.com>
2023-09-24 13:49:55 -0300

    Create Deposito.java

---
+++ atividade6/Deposito.java
@@ -0,0 +1,11 @@
+public class Deposito {
+    private float valor;
+
+    public Deposito(float valor) {
+        this.valor = valor;
+    }
+
+    public float getValor() {
+        return valor;
+    }
+}
