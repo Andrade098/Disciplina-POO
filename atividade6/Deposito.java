f0d8cca540f363eb47e48137b9e30fc31874b38a
Lucas Andrade Torres <127760173+Andrade098@users.noreply.github.com>
2023-09-24 13:52:45 -0300

    Update Deposito.java

--- atividade6/Deposito.java
+++ atividade6/Deposito.java
@@ -2,10 +2,12 @@ public class Deposito {
     private float valor;
 
     public Deposito(float valor) {
+        // Construtor da classe Deposito
         this.valor = valor;
     }
 
     public float getValor() {
+        // Retorna o valor do depósito
         return valor;
     }
 }
